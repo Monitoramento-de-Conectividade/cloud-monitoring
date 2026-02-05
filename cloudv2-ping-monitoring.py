25937cc152255b0de86d62ed8f979a58965c65ad
EduCR03 <eduardocostar03@gmail.com>
2026-02-05 15:47:48 -0300

    Analises

--- cloudv2-ping-monitoring.py
+++ cloudv2-ping-monitoring.py
@@ -13,12 +13,12 @@ import time        # [NOVO] para pequenas esperas/controlar timeout
 BROKER = os.environ.get("BROKER", "a19mijesri84u2-ats.iot.us-east-1.amazonaws.com")
 PORT = int(os.environ.get("PORT", 8883))
 
-TOPICS = ["cloudv2-ping", "cloud2", "cloudv2", "cloudv2-shutdown", "hydrometer-cloudv2", "hydrometer-cloud2", "icrop-cloudv2", "cloudv2-network"]
+TOPICS = ["cloudv2-ping", "cloud2", "cloudv2", "cloudv2-shutdown", "hydrometer-cloudv2", "hydrometer-cloud2", "icrop-cloudv2", "cloudv2-network", "configurado", "padrao"]
 
-FILTER_NAMES = ["PioneiraLEM_2", "NovaBahia_6", "Savana_16", "ItacirJunior_5", "soilteste_1", "soilteste_2", "OldFriends_12", "GrupoBB_2", "Paineira_16"] 
+FILTER_NAMES = ["PioneiraLEM_1", "NovaBahia_6", "Savana_16", "ItacirJunior_5", "soilteste_1", "soilteste_2", "TerraNostra_4", "GrupoBB_2", "Paineira_16", "Dileta_1"] 
 
 # [NOVO] Tópicos da funcionalidade #11$
-CMD_TOPIC  = os.environ.get("CMD_TOPIC",  "soilteste_2")    # onde publicaremos "#11$"
+CMD_TOPIC  = os.environ.get("CMD_TOPIC",  "PioneiraLEM_1")    # onde publicaremos "#11$"
 INFO_TOPIC = os.environ.get("INFO_TOPIC", "cloudv2-info")   # onde ouviremos a resposta
 
 # [NOVO] Intervalo aleatório entre envios (minutos)
